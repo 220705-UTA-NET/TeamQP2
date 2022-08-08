c1f27ed64b06da75bb7cf05aa64aab1e23cb2cfc
bsassano <49253961+bsassano@users.noreply.github.com>
2022-08-08 11:49:58 -0700

    Update Program.cs

--- P1/GameReviewAPI/GameReviewAPI.API/Program.cs
+++ P1/GameReviewAPI/GameReviewAPI.API/Program.cs
@@ -3,8 +3,6 @@ using GameReviewAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string connectionString = "Server=tcp:myfirstdatabase-brandon.database.windows.net,1433;Initial Catalog=myfirstdatabase;Persist Security Info=False;User ID=bsassano;Password=Bts2000!!!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
