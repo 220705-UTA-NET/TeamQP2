39538661bb805fce1cf7b0eadedd9c7658256d21
bsassano <49253961+bsassano@users.noreply.github.com>
2022-08-10 12:46:06 -0700

    Revert "fixed merge issues"

--- angular-brandons-gaming-emporium/src/app/app-routing.module.ts
+++ angular-brandons-gaming-emporium/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AddGameComponent } from './add-game/add-game.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 import { GameReviewComponent } from './game-review/game-review.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Games', pathMatch: 'full' },
-  { path: 'Games', component: AddGameComponent },
+  { path: 'Games', component: HeroFormComponent },
   { path: 'Review', component: GameReviewComponent }
 ];
 
